8ff519fd5e3e65ed08e9e798ca5827cc06a8bee9
bioback <78535998+bioback@users.noreply.github.com>
2021-03-24 15:19:29 +0100

    updated footer

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,14 +1,78 @@
-import { Card, Container } from "react-bootstrap"
+import { Card, Col, Container, Row } from "react-bootstrap"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faFacebookSquare, faTwitter, faInstagram} from "@fortawesome/free-brands-svg-icons"
+import {faFacebookSquare, faTwitter, faInstagram, faLinkedin, faCcVisa, faCcPaypal, faCcMastercard, faCcApplePay, faCcAmex, faCcDiscover} from "@fortawesome/free-brands-svg-icons"
 
 const MyFooter = () =>(
     <Card.Footer className="text-muted">
         <Container>
-        <FontAwesomeIcon icon={faFacebookSquare}/>
-        <FontAwesomeIcon icon={faTwitter}/>
-        <FontAwesomeIcon icon={faInstagram}/>
-        My Foot
+        <Row>
+            <Col className="d-flex justify-content-start">
+                <p>You can find us on:</p>
+                <FontAwesomeIcon className="mr-3 ml-3" icon={faFacebookSquare}/>
+                <FontAwesomeIcon className="mr-3" icon={faTwitter}/>
+                <FontAwesomeIcon className="mr-3" icon={faInstagram}/>
+                <FontAwesomeIcon className="mr-3" icon={faLinkedin}/>
+            </Col>
+            <Col className="d-flex justify-content-end">
+            <p>REACT BOOKSTORE</p>
+            </Col>
+        </Row>
+        <Row className="my-3">
+            <Col>
+                <ul class="list-group">
+                    <h5>Business</h5>
+                    <li>Discover React!</li>
+                    <li>Events</li>
+                    <li>Open a franchising</li>
+                    <li>Affilations</li>
+                    <li>Our Promotions</li>
+                    <li>Work with us</li>
+                </ul>
+            </Col>
+            <Col>
+                <ul class="list-group">
+                    <h5>Our services</h5>
+                    <li>Pre-order & pickup</li>
+                    <li>APP</li>
+                    <li>Gift Cards</li>
+                    <li>Special Orders</li>
+                    <li>Online collaborations</li>
+                    <li>Print online photos</li>
+                </ul>
+            </Col>
+            <Col>
+                <ul class="list-group">
+                    <h5>Client area</h5>
+                    <li>Contact us</li>
+                    <li>FAQ</li>
+                    <li>Selling conditions</li>
+                    <li>Members card</li>
+                    <li>Privacy</li>
+                    <li>Cookies and navigation data</li>
+                </ul>
+            </Col>
+            <Col>
+                <ul class="list-group">
+                    <h5>Friend websites</h5>
+                    <li>Github</li>
+                    <li>React</li>
+                    <li>Strive School</li>
+                    <li>Bootstrap</li>
+                    <li>DuckDuckGo</li>
+                </ul>
+            </Col>
+        </Row>
+        <Row className="mx-auto">
+            <Col>
+            <p>We accept the following payment methods: </p>
+            <FontAwesomeIcon className="mr-3 ml-3" icon={faCcVisa}/>
+            <FontAwesomeIcon className="mr-3" icon={faCcMastercard}/>
+            <FontAwesomeIcon className="mr-3" icon={faCcDiscover}/>
+            <FontAwesomeIcon className="mr-3" icon={faCcAmex}/>
+            <FontAwesomeIcon className="mr-3" icon={faCcPaypal}/>
+            <FontAwesomeIcon className="mr-3" icon={faCcApplePay}/>
+            </Col>
+        </Row>
         </Container>
     </Card.Footer>
 )
--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,26 +1,32 @@
-import { Navbar, Nav, NavDropdown } from "react-bootstrap"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faBook} from "@fortawesome/free-solid-svg-icons"
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
 const MyNav = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home"><FontAwesomeIcon icon={faBook}/> React-Bookstore</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">About</Nav.Link>
-        <NavDropdown title="Browse" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Fantasy</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">History</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Horror</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.4">Romance</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Sci-Fi</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">All categories</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  <Container>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">
+        <FontAwesomeIcon icon={faBook} /> React-Bookstore
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">About</Nav.Link>
+          <NavDropdown title="Browse" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Fantasy</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">History</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Horror</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">Romance</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Sci-Fi</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              All categories
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Container>
 );
 
-export default MyNav
+export default MyNav;
