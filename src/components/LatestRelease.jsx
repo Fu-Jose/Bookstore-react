1cfcd85f11c4f57bdea9f58ec1f03cbb235da376
bioback <78535998+bioback@users.noreply.github.com>
2021-03-24 11:52:09 +0100

    fixed buy button, added ellipsis

--- src/components/LatestRelease.jsx
+++ src/components/LatestRelease.jsx
@@ -1,22 +1,40 @@
 import React from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import items from "../data/horror.json";
-
+import EllipsisText from "react-ellipsis-text";
 class LatestRelease extends React.Component {
   render() {
     return (
       <Container>
         <Row>
           {items.map((item) => (
-            <Col xs={4} sm={4} md={3} lg={2}>
-            <Card style={{ width: "10rem"}} className="my-3">
-              <Card.Img style={{ width: "10rem", height: "15rem" }} variant="top" src={item.img} />
-              <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>{item.price} €</Card.Text>
-                <Button variant="primary">BUY!</Button>
-              </Card.Body>
-            </Card>
+            <Col key={item.asin} xs={4} sm={4} md={3} lg={2}>
+              <Card
+                style={{ width: "10rem", height: "27rem" }}
+                className="my-3"
+              >
+                <Card.Img
+                  style={{ width: "10rem", height: "15rem" }}
+                  variant="top"
+                  src={item.img}
+                />
+                <Card.Body>
+                  <Card.Title>
+                    <EllipsisText text={item.title} length={20} />
+                  </Card.Title>
+                  <Card.Text>{item.price} €</Card.Text>
+                  <Button
+                    style={{
+                      position: "absolute",
+                      left: "50px",
+                      bottom: "20px",
+                    }}
+                    variant="primary"
+                  >
+                    BUY!
+                  </Button>
+                </Card.Body>
+              </Card>
             </Col>
           ))}
         </Row>
